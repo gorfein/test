4ce87cffef5e6792895e6455cab51dbb0ea14105
xintest <gorfein1@gmail.com>
2014-05-21 13:00:14 +0800

    test 3

--- src/cn/outsourceit/servermonitor.java
+++ src/cn/outsourceit/servermonitor.java
@@ -25,7 +25,6 @@ import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
 /**
  * Created with IntelliJ IDEA.
